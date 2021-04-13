a18aa8d6a4780b397ded870b6adf799d77119128
pgzmnk <pgzmnk@gmail.com>
2021-04-12 18:13:14 -0600

    Structure unittests so they run. (#5)

--- tests/hooks/test_hightouch_hook.py
+++ tests/hooks/test_hightouch_hook.py
@@ -1,4 +1,18 @@
+"""
+Unittest module to test Hightouch Hook.
+
+Requires the unittest and requests-mock Python libraries.
+
+Run test:
+
+    python3 -m unittest tests.hooks.test_hightouch_hook.TestHightouchHook
+
+"""
+
+import requests_mock
+import unittest
 from unittest import mock
+
 
 from airflow_provider_hightouch.hooks.hightouch import HightouchHook
 
@@ -7,7 +21,9 @@ from airflow_provider_hightouch.hooks.hightouch import HightouchHook
     "os.environ",
     AIRFLOW_CONN_HIGHTOUCH_DEFAULT="http://https%3A%2F%2Ftest.hightouch.io%2F",
 )
-class TestHightouchHook:
+class TestHightouchHook(unittest.TestCase):
+
+    @requests_mock.mock()
     def test_hightouch_get_sync(self, requests_mock):
         requests_mock.get(
             "https://test.hightouch.io/api/v1/rest/syncStatus/1", json={"success": True}
@@ -16,6 +32,7 @@ class TestHightouchHook:
         response = hook.get_sync_status(1)
         assert response.json()["success"]
 
+    @requests_mock.mock()
     def test_hightouch_submit_sync(self, requests_mock):
         requests_mock.post(
             "https://test.hightouch.io/api/v1/rest/triggerSync/1",

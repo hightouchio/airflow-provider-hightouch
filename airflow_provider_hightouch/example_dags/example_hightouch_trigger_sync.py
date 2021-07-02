e606aa3a92c68ebde153864a6003b9c2936061fb
Pedram Navid <pedram@hightouch.io>
2021-07-02 12:37:08 -0700

    Update Airflow operator use the v2 Hightouch API for synchronous polling

--- airflow_provider_hightouch/example_dags/example_hightouch_trigger_sync.py
+++ airflow_provider_hightouch/example_dags/example_hightouch_trigger_sync.py
@@ -17,6 +17,16 @@ with DAG(
 ) as dag:
 
     latest_only = LatestOnlyOperator(task_id="latest_only", dag=dag)
-    run_sync = HightouchTriggerSyncOperator(task_id="run_my_sync", sync_id=1)
+
+    # This task runs async, and doesn't poll for status or fail on error
+    run_async = HightouchTriggerSyncOperator(task_id="run_async", sync_id=4)
+
+    # This tasks polls the API until the Hightouch Sync completes or errors.
+    # Warnings are considered errors, but this can be turned off with the
+    # specified flag
+    run_sync = HightouchTriggerSyncOperator(
+        task_id="run_sync", sync_id=5, synchronous=True, error_on_warning=True
+    )
 
     latest_only >> run_sync
+    latest_only >> run_async
--- tests/hooks/test_hightouch_hook.py
+++ tests/hooks/test_hightouch_hook.py
@@ -9,10 +9,10 @@ Run test:
 
 """
 
-import requests_mock
 import unittest
 from unittest import mock
 
+import requests_mock
 
 from airflow_provider_hightouch.hooks.hightouch import HightouchHook
 
@@ -22,22 +22,22 @@ from airflow_provider_hightouch.hooks.hightouch import HightouchHook
     AIRFLOW_CONN_HIGHTOUCH_DEFAULT="http://https%3A%2F%2Ftest.hightouch.io%2F",
 )
 class TestHightouchHook(unittest.TestCase):
-
     @requests_mock.mock()
-    def test_hightouch_get_sync(self, requests_mock):
+    def test_hightouch_get_sync_status(self, requests_mock):
         requests_mock.get(
-            "https://test.hightouch.io/api/v1/rest/syncStatus/1", json={"success": True}
+            "https://test.hightouch.io/api/v2/rest/sync/1",
+            json={"sync": {"sync_status": "success"}},
         )
-        hook = HightouchHook()
+        hook = HightouchHook(api_version="v2")
         response = hook.get_sync_status(1)
-        assert response.json()["success"]
+        assert response.json()["sync"]["sync_status"] == "success"
 
     @requests_mock.mock()
     def test_hightouch_submit_sync(self, requests_mock):
         requests_mock.post(
-            "https://test.hightouch.io/api/v1/rest/triggerSync/1",
-            json={"success": True},
+            "https://test.hightouch.io/api/v2/rest/run/1",
+            json={"sync": {"sync_status": "success"}},
         )
         hook = HightouchHook()
         response = hook.submit_sync(1)
-        assert response.json()["success"]
+        assert response.json()["sync"]["sync_status"] == "success"
